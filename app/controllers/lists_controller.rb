ba5ea854fe793993bdc78f11223eac145fb2fd91
reszcb740 <reszcb740@Gmail.com>
2024-08-04 12:52:20 +0000

    edit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,7 +8,7 @@ class ListsController < ApplicationController
 
    list.save
 
-   redirect_to '/top'
+   redirect_to list_path(list.id)
   end
   def index
     @lists = List.all
@@ -19,8 +19,14 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
 
   private
 
